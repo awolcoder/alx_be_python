5a2111ca86027fd0577a3b9dd1c2139f7e4708e3
awolcoder <abdulbasitawil42@gmail.com>
2025-07-26 18:52:46 +0300

    Fncs and data strc program

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+def perform_operation(num1, num2, operation):
+    """
+    Perform basic arithmatic operations.
+
+    Parameters:
+        num1 (float): First number
+        num2 (float): Second number
+        operation (str): Operation to perform: 'add', 'subtract', 'multiply', or 'divide'
+
+    Returns:
+        float or str: Result of the operation or error message for division by zero
+    """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
+    
+    
+    
+
